74081ae04ef6d8517b54a4ed4af3ac893e787335
ShynRdi <rdishyn@gmail.com>
2023-06-03 09:16:10 +0330

    I guess this is our last commit on this branch

--- assets/scripts/homeScript.js
+++ assets/scripts/homeScript.js
@@ -20,26 +20,26 @@ $(window).scroll(function () {
 
 // console.log();
 
-function storeFile() {
-  const fileInput = document.getElementById("fileInput");
-  const file = fileInput.files[0];
-  if (file) {
-    const formData = new FormData();
-    formData.append("file", file);
-    fetch("http://localhost:3002/upload", {
-      headers: { "Content-Type": "multipart/form-data" },
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.text())
-      .then((result) => {
-        console.log("File stored:", result);
-      })
-      .catch((error) => {
-        console.error("Error storing file:", error);
-      });
-  }
-}
+// function storeFile() {
+//   const fileInput = document.getElementById("fileInput");
+//   const file = fileInput.files[0];
+//   if (file) {
+//     const formData = new FormData();
+//     formData.append("file", file);
+//     fetch("http://localhost:3002/upload", {
+//       headers: { "Content-Type": "multipart/form-data" },
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((response) => response.text())
+//       .then((result) => {
+//         console.log("File stored:", result);
+//       })
+//       .catch((error) => {
+//         console.error("Error storing file:", error);
+//       });
+//   }
+// }
 
 //
 const libraryChecker = document.getElementsByClassName("books")[0];
@@ -83,7 +83,7 @@ if (!libraryChecker.innerHTML) {
         book.classList.add("book");
         libraryChecker.appendChild(book);
 
-        console.log(item.image);
+        // console.log(item.image);
       });
     })
     .catch((error) => {
@@ -110,3 +110,60 @@ console.log(1);
 //     reader.readAsDataURL(file);
 //   }
 // }
+
+function handelSubmit(e) {
+  e.preventDefault();
+  axios.post("http://localhost:3002/books", {
+    image: `${document.getElementById("bookImage").value}`,
+    title: `${document.getElementById("bookName").value}`,
+    details: `${document.getElementById("bookDetails").value}`,
+  });
+}
+// var myHeaders = new Headers();
+// myHeaders.append("token", "");
+// const fileInput = document.querySelector("input[type='file']");
+// myHeaders.append("Content-Type", "multipart/form-data");
+// console.log(fileInput.files[0]);
+// var formdata = new FormData();
+// formdata.append("image", fileInput.files[0]);
+
+// var requestOptions = {
+//   method: "POST",
+//   headers: myHeaders,
+//   body: formdata,
+//   redirect: "follow",
+// };
+
+// fetch("http://localhost:3002/upload", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
+// const axios = require('axios');
+// const FormData = require("form-data");
+// const fs = require("fs");
+// let data = new FormData();
+// data.append(
+//   "image",
+//   document.getElementById("fileInput").files[0]
+//   // fs.createReadStream("/home/user/Documents/computer sciencs/report/test.pdf")
+// );
+
+// let config = {
+//   method: "post",
+//   maxBodyLength: Infinity,
+//   url: "http://localhost:3002/upload",
+//   headers: {
+//     // token: "",
+//     // ...data.getHeaders(),
+//   },
+//   data: data,
+// };
+
+// axios
+//   .request(config)
+//   .then((response) => {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
